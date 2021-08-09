659678b580f0fe97e732fd99a942328c7cb53a81
tk-codelab <tk.codelab@gmail.com>
2021-08-09 10:37:16 +0200

    Fix: needs to be on load, not on DOMContentLoaded

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-window.addEventListener('DOMContentLoaded', (event) => {
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images. 
+window.addEventListener('load', (event) => {
     let magicGrid = new MagicGrid({ // A simple, lightweight Javascript library for dynamic grid layouts. https://github.com/e-oj/Magic-Grid
         container: ".image-grid", // Required. Can be a class, id, or an HTMLElement.
         static: true, // Required for static content.
