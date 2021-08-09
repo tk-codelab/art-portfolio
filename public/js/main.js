3b7cbbef38eee9d4737d8a3610defda2ddfc10a8
tk-codelab <tk.codelab@gmail.com>
2021-08-09 10:23:22 +0200

    Fix: images squased at top of window on reload

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
-let magicGrid = new MagicGrid({
-    container: ".image-grid", // Required. Can be a class, id, or an HTMLElement.
-    static: true, // Required for static content.
-    animate: true, // Optional.
-    gutter: 0,
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+window.addEventListener('DOMContentLoaded', (event) => {
+    let magicGrid = new MagicGrid({ // A simple, lightweight Javascript library for dynamic grid layouts. https://github.com/e-oj/Magic-Grid
+        container: ".image-grid", // Required. Can be a class, id, or an HTMLElement.
+        static: true, // Required for static content.
+        animate: true, // Optional.
+        gutter: 0,
+    });
+      
+    magicGrid.listen();
 });
-  
-magicGrid.listen();
-
