134d2cb5504deb9b6da91d3986afc4affe7feae9
tk-codelab <tk.codelab@gmail.com>
2021-10-30 12:40:54 +0200

    Feat: new work. Fix: small css fixes.

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ node-sass - to compile scss into css
 */
 
 // we require the express library from node.modules:
-const express = require('express'); 
+const express = require('express');
 // calling the returned object to create an Express application. We can then access the properties and functions of the application object:
-const app = express(); 
+const app = express();
 app.use(express.static('public'));
 
 // We require the file system library that's part of node:
-const fs = require('fs'); 
+const fs = require('fs');
 var ejs = require('ejs');
 var path = require('path');
 
@@ -24,22 +24,26 @@ app.set('view engine', 'html');
 
 // Run the server in a bash command prompt by calling: node app.js
 // Defined a (callback) route handler function for HTTP GET requests to the site root ('/'):
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index')
 });
 
-app.get('/contact/', function(req, res) {
+app.get('/contact/', function (req, res) {
     res.render('contact')
 });
 
-app.get('/portrait-alla-rembrandt/', function(req, res) {
+app.get('/portrait-alla-rembrandt/', function (req, res) {
     res.render('portrait-alla-rembrandt')
 });
 
-app.get('/all-sections/', function(req, res) {
+app.get('/the-rower/', function (req, res) {
+    res.render('the-rower')
+});
+
+app.get('/all-sections/', function (req, res) {
     res.render('all-sections')
 });
-  
+
 // app.listen(3231, function() {
 //     console.log('art portfolio app listening on port 3231!');
 // });
@@ -47,7 +51,7 @@ app.get('/all-sections/', function(req, res) {
 // Setup for Heroku
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3231;
-  console.log('art portfolio app listening on port 3231!');
+    port = 3231;
+    console.log('art portfolio app listening on port 3231!');
 }
 app.listen(port);
